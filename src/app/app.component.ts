0507668297523964a00074ab588770c257ed423f
Waysker <wojtek98@gmail.com>
2020-03-16 21:13:55 +0100

    initial withc working image dropdown

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,11 @@
 import { Component } from '@angular/core';
 
+export interface Item {
+  value: string;
+  viewValue: string;
+  img: string;
+}
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -7,4 +13,10 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'test';
+  selectedItem: Item= { value: "", viewValue: "", img: "" };
+  items: Item[] = [
+    { value: 'Computer', viewValue: 'Pizza', img: 'assets/images/computer.png' },
+    { value: 'Plastic', viewValue: 'Steak', img: 'assets/images/plastic.png' },
+    { value: 'tacos-2', viewValue: 'Tacos', img: 'https://www.akberiqbal.com/favicon-16x16.png' }
+  ];
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,8 +1,11 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+import {MatSliderModule} from '@angular/material/slider'
+import {MatSelectModule} from '@angular/material/select'; 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations'
 
 @NgModule({
   declarations: [
@@ -10,7 +13,10 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    MatSelectModule,
+    MatSliderModule,
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
